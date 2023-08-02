c7f3202faa33ea862b2b799b9ad814681fc0cca2
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-08-02 09:26:45 +0900

    add 料理作り方画面

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/models/meal.dart';
+
+class MealDetails extends StatelessWidget {
+  const MealDetails({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                "ingredients",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.w700,
+                    ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              for (final ingredient in meal.ingredients)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 4),
+                  child: Text(
+                    ingredient,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              const SizedBox(
+                height: 16,
+              ),
+                Text(
+                "Step",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.w700,
+                    ),
+              ),
+               const SizedBox(
+                height: 8,
+              ),
+              for (final step in meal.steps)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8),
+                  child: Text(
+                    step,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
